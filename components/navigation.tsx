b3681097cebc9d360f670905222e73efdff4b70c
stanislaus-onwuka <kachi2505@yahoo.com>
2023-03-07 18:12:11 +0100

    Homepage v1 in progress

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,15 +5,17 @@ import { HideOn } from "react-hide-on-scroll"
 const Navigation = () => {
     return (
         <nav className='flex items-center justify-between px-[4%] pt-5 pb-7 w-full bg-mystery-900 lg:bg-transparent lg:px-[120px] fixed z-20 top-0' id='nav'>
-            <div className='flex'>
-                <img
-                    src={'/assets/svg/navigation/logo.svg'}
-                    alt='Noti logo'
-                />
-                <HideOn divID="homepage" atDiv={true} showOnPageInit>
-                    <h2 className='text-[17px] leading-[27px] self-end text-mystery-300 ml-4 font-[450] hidden md:block'>Thenoticompany</h2>
-                </HideOn>
-            </div>
+            <Link href={"/"}>
+                <div className='flex'>
+                    <img
+                        src={'/assets/svg/navigation/logo.svg'}
+                        alt='Noti logo'
+                    />
+                    <HideOn divID="main" atDiv={true} showOnPageInit>
+                        <h2 className='text-[17px] leading-[27px] self-end text-mystery-300 ml-4 font-[450] hidden md:block'>Thenoticompany</h2>
+                    </HideOn>
+                </div>
+            </Link>
             <ul className="font-futura hidden lg:flex">
                 <li className="p-2 text-mystery-300">
                     <Link href="/about"> About </Link>
